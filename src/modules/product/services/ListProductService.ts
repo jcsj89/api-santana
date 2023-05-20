3252c62fc5d16d540d727d4452a4dbb17d1e8855
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2023-05-21 00:02:14 +0100

    21.05.23

--- src/modules/product/services/ListProductService.ts
+++ src/modules/product/services/ListProductService.ts
@@ -3,12 +3,16 @@ import Product from '../model/ProductModel';
 
 export default class ListProductService {
   public async execute() {
-    const products: Product[] = await knex('products').select('*');
+    const products: Product[] = await knex('products')
+      .select('*')
+      .orderBy('updated_at', 'desc');
 
     if (products.length < 1) {
       return { MESSAGE: 'Product not found.' };
     }
 
-    return products;
+    const productsActives = products.filter((prod) => prod.active);
+
+    return productsActives;
   }
 }
