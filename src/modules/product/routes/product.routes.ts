4955f22437593f3eb35f56cec6e12fc2147181b9
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2023-06-17 13:18:44 +0100

    17.06.23

--- src/modules/product/routes/product.routes.ts
+++ src/modules/product/routes/product.routes.ts
@@ -7,18 +7,26 @@ import postProductValidation from '../validation/postProductValidation';
 const productController = new ProductController();
 const productRoutes = Router();
 
+// list products
 productRoutes.get('/products', productController.list);
+
+// list one product by id
+productRoutes.get('/products/:id', productController.list);
+
+// delete one products
 productRoutes.delete(
   '/products/:id',
   celebrate(deleteProductValidation()),
   productController.delete,
 );
+// create a product
 productRoutes.post(
   '/products',
   celebrate(postProductValidation()),
   productController.create,
 );
 
+// update a products
 productRoutes.put('/products/:id', productController.update);
 
 export default productRoutes;
